52c5f1379562e8dac6422e575c671f6077dd3692
Iliyas Patait <pataitiliyas@gmail.com>
2023-12-13 06:13:27 +0000

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("ibm.prod.ip.manageprod.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
